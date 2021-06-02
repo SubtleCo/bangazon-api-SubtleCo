42c8f04e553464652f24a22c0922a157bee0b40c
Alex Martin <alex@subtleco.co>
2021-06-02 15:40:18 -0500

    fixed the DB off by 1 issue, Report - favorited sellers

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import expensive_products, completed_orders, incomplete_orders
+from .views import expensive_products, completed_orders, incomplete_orders, favorited_sellers
 
 urlpatterns = [
     path('reports/expensiveproducts', expensive_products),
     path('reports/orders/completed', completed_orders),
     path('reports/orders/incomplete', incomplete_orders),
+    path('reports/customers/favorites', favorited_sellers)
 ]
--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -2,3 +2,4 @@ from .connection import Connection
 from .products.expensive_products import expensive_products
 from .orders.completed_orders import completed_orders
 from .orders.incomplete_orders import incomplete_orders
+from .customers.favorited_sellers import favorited_sellers
