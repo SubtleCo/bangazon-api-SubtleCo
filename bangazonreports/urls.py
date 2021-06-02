aab0a6716202e5d23998c20b0b2c55a8115a3398
Alex Martin <alex@subtleco.co>
2021-06-02 13:49:11 -0500

    Report - inexpensive products

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import expensive_products
+from .views import expensive_products, cheap_products
 
 urlpatterns = [
-    path('reports/expensiveproducts', expensive_products)
+    path('reports/expensiveproducts', expensive_products),
+    path('reports/cheapproducts', cheap_products),
 ]
